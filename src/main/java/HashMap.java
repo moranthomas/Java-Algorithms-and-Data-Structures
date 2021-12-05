1c3cc31d542310f8a42b22834b30670f05a3cc8c
moranthomas <thomas@optimizme.com>
2021-12-05 17:15:02 +0000

    hashmaps

--- src/main/java/HashMap.java
+++ src/main/java/HashMap.java
@@ -1,3 +1,30 @@
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
 public class HashMap {
+
+    public class Product {
+        String productName;
+        String productDescription;
+
+        public Product(String productName, String productDescription) {
+            this.productName = productName;
+            this.productDescription = productDescription;
+        }
+    }
+
+    public void createNewHashMap() {
+
+        Map<String, Product> productsByName = (Map<String, Product>) new HashMap();
+
+        //Add products to the Map
+        Product eBike = new Product("E-Bike", "A bike with a battery");
+        Product roadBike = new Product("Road bike", "A bike for competition");
+       /* productsByName.put(eBike.getName(), eBike);
+        productsByName.put(roadBike.getName(), roadBike);*/
+    }
+
 
 }
